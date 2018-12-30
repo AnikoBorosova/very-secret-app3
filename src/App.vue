9b193529fc360004c24ccd1856016f6c21d15961
Anikó Borosová <borosanikoo@gmail.com>
2018-12-30 16:18:43 +0100

    post request

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-form ref="form" class="ml-5">
-          <v-text-field label="amount spent"></v-text-field>
+          <v-text-field label="amount spent" v-model="amount"></v-text-field>
          
           <v-btn @click="submit">submit</v-btn>
     </v-form>
@@ -9,16 +9,36 @@
 </template>
 
 <script>
+const request = require("superagent");
+const config = require("./config-dev.json");
+
 export default {
   name: 'App',
   data() {
     return {
-
+      amount: "",
     };
   },
   methods: {
     submit() {
+      debugger;
+      const expensesUrl = config.routes.backendRoute + "/expenses";
+      const userExpenses = this.amount;
 
+      request.post(expensesUrl)
+          .withCredentials()
+          .set("Content-Type", "application/json")
+          .send(userExpenses)
+          .end((err, res) => {
+
+            if (err) {
+              console.log(err);
+              return;
+            }
+
+            console.log(res.body);
+
+          })
     }
   }
 }
